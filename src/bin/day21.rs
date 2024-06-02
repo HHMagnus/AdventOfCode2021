226ccdfafb830f1a187210e5db69391f8f955c10
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-06-02 22:28:39 +0200

    day 21 part 2

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn main() {
     let input = fs::read_to_string("input/day21.txt")
@@ -12,6 +12,59 @@ fn main() {
 	let part1 = deterministic_100_sided_die(first, second);
 
 	println!("Day 21 part 1: {}", part1);
+
+	let mut memo = HashMap::new();
+
+	let part2 = dirac_dice(&mut memo, first, second, 0, 0, true);
+
+	println!("Day 21 part 2: {}", part2.0.max(part2.1));
+}
+
+fn dirac_dice(memo: &mut HashMap<(u8, u8, u64, u64, bool), (u128, u128)>, f: u8, s: u8, fs: u64, ss: u64, t: bool) -> (u128, u128) {
+	let mut w1 = 0;
+	let mut w2 = 0;
+
+	let key = (f, s, fs, ss, t);
+	if memo.contains_key(&key) {
+		return memo[&(f, s, fs, ss, t)];
+	}
+
+	for r1 in 1..4 {
+		for r2 in 1..4 {
+			for r3 in 1..4 {
+				let roll = r1 + r2 + r3;
+
+				if t {
+					let f = f + roll;
+					let f = (f - 1) % 10 + 1;
+					let fs = fs + f as u64;
+
+					if fs >= 21 {
+						w1 += 1;
+					} else {
+						let (e1, e2) = dirac_dice(memo, f, s, fs, ss, !t);
+						w1 += e1;
+						w2 += e2;
+					}
+				} else {
+					let s = s + roll;
+					let s = (s - 1) % 10 + 1;
+					let ss = ss + s as u64;
+
+					if ss >= 21 {
+						w2 += 1;
+					} else {
+						let (e1, e2) = dirac_dice(memo, f, s, fs, ss, !t);
+						w1 += e1;
+						w2 += e2;
+					}
+				}
+			}
+		}
+	}
+	let value = (w1, w2);
+	memo.insert(key, value);
+	value
 }
 
 fn deterministic_100_sided_die(first: u8, second: u8) -> u64 {
